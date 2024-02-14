14eab92a3ade862182f8fad6511bbc5ceaa69e01
Fox Snowpatch <fox@fox.ozlabs.org>
2024-02-14 09:28:35 +0000

    From patchwork series 394930
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=394930

--- arch/powerpc/include/asm/percpu.h
+++ arch/powerpc/include/asm/percpu.h
@@ -15,6 +15,16 @@
 #endif /* CONFIG_SMP */
 #endif /* __powerpc64__ */
 
+#ifdef CONFIG_PPC64
+#include <linux/jump_label.h>
+DECLARE_STATIC_KEY_FALSE(__percpu_embed_first_chunk);
+
+#define is_embed_first_chunk	\
+		(static_key_enabled(&__percpu_embed_first_chunk.key))
+#else
+#define is_embed_first_chunk	(true)
+#endif /* CONFIG_PPC64 */
+
 #include <asm-generic/percpu.h>
 
 #include <asm/paca.h>
