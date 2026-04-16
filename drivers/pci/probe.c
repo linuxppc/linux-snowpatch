3c5a0a5490e7874a3274b6c78a6917ab2eda05b2
Fox Snowpatch <fox@fox.ozlabs.org>
2026-04-16 18:04:46 +0000

    From patchwork series 500194
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=500194

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -1068,9 +1068,6 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 			dev_err(&bus->dev, "failed to add bus: %d\n", err);
 	}
 
-	/* Create legacy_io and legacy_mem files for this bus */
-	pci_create_legacy_files(bus);
-
 	if (parent)
 		dev_info(parent, "PCI host bridge to bus %s\n", name);
 	else
@@ -1275,9 +1272,6 @@ add_dev:
 		if (WARN_ON(ret < 0))
 			dev_err(&child->dev, "failed to add bus: %d\n", ret);
 	}
-
-	/* Create legacy_io and legacy_mem files for this bus */
-	pci_create_legacy_files(child);
 
 	return child;
 }
